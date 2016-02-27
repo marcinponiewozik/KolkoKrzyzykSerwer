0f89fc7cc3fa81a308428c967dfb15e698eb522f
Marcin <Marcin@192.168.1.20>
2016-02-27 17:11:45 +0100

    add jaxrs

--- src/main/java/entitys/Gra.java
+++ src/main/java/entitys/Gra.java
@@ -29,7 +29,7 @@ public class Gra implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     
-    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.REMOVE)
+    @ManyToOne(fetch = FetchType.EAGER,cascade = CascadeType.REMOVE)
     private Osoba gospodarz;
     @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.REMOVE)
     private Osoba przeciwnik;
